a65ab2e889544f2281ad45c41d01bfeb6c08e758
Mazuru Gabriel <gabrielmazuru6@gmail.com>
2025-07-02 18:36:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,10 +8,25 @@ function getRandomInt(max) {
 
 function rollDice() {
   let randomNumber = getRandomInt(6) + 1;
+  let randomNumber2 = getRandomInt(6) + 1;
   let diceImage = document.getElementById("dice");
+  let diceImage2 = document.getElementById("dice2");
   if (randomNumber % 2 == 0) {
     diceImage.src = `Dice-${randomNumber}.svg`;
+    diceImage2.src = `Dice-${randomNumber2}.svg`;
   } else {
     diceImage.src = `Dice-${randomNumber}.svg.png`;
+    diceImage2.src = `Dice-${randomNumber2}.svg.png`;
+  }
+  whoWon(randomNumber, randomNumber2);
+}
+
+function whoWon(randomNumber, randomNumber2) {
+  if (randomNumber > randomNumber2) {
+    alert("Congrats, you won!");
+  } else if (randomNumber == randomNumber2) {
+    alert("It's tie!");
+  } else {
+    alert("You lost!");
   }
 }
